7b10f553695878f72a4c4345a1abddc117dee7ac
HENRY <prez.gumiel@gmail.com>
2019-12-25 17:00:52 -0400

    9886124-23

--- ejemplo/tcpdf_ci/application/libraries/pdf/PdfCI.php
+++ ejemplo/tcpdf_ci/application/libraries/pdf/PdfCI.php
@@ -1,6 +1,6 @@
 <?php 
 require_once APPPATH.'third_party/TCPDF/tcpdf.php';
-class PdfCI {
+class Pdfci {
    	//funciones que queremos implementar en Miclase.
 	public $TCPDF;
 	
@@ -15,42 +15,42 @@ class PdfCI {
 	public function generarCabecera() // aqui se configurara todos los pdf`s
 	{
 		// set document information
-		$pdf->SetCreator(PDF_CREATOR);
-		$pdf->SetAuthor('Nicola Asuni');
-		$pdf->SetTitle('TCPDF Example 006');
-		$pdf->SetSubject('TCPDF Tutorial');
-		$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+		$this->TCPDF->SetCreator(PDF_CREATOR);
+		$this->TCPDF->SetAuthor('Nicola Asuni');
+		$this->TCPDF->SetTitle('TCPDF Example 006');
+		$this->TCPDF->SetSubject('TCPDF Tutorial');
+		$this->TCPDF->SetKeywords('TCPDF, PDF, example, test, guide');
 
 		// set default header data
-		$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
+		$this->TCPDF->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
 
 		// set header and footer fonts
-		$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-		$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+		$this->TCPDF->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+		$this->TCPDF->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 
 		// set default monospaced font
-		$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+		$this->TCPDF->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
 		// set margins
-		$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-		$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-		$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+		$this->TCPDF->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+		$this->TCPDF->SetHeaderMargin(PDF_MARGIN_HEADER);
+		$this->TCPDF->SetFooterMargin(PDF_MARGIN_FOOTER);
 
 		// set auto page breaks
-		$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+		$this->TCPDF->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 		// set image scale factor
-		$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+		$this->TCPDF->setImageScale(PDF_IMAGE_SCALE_RATIO);
 
 		
 
 		// ---------------------------------------------------------
 
 		// set font
-		$pdf->SetFont('dejavusans', '', 10);
+		$this->TCPDF->SetFont('dejavusans', '', 10);
 
 		// add a page
-		$pdf->AddPage();
+		$this->TCPDF->AddPage();
 	}
 
 	public function generarPieDePagina()
